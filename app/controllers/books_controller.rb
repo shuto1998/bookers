d3685fce079e5ab4bb238aeef167a0c3c9ffa28e
shuto1998 <shuto.y0319@gmail.com>
2023-02-20 13:02:06 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -24,7 +24,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
   end
 
