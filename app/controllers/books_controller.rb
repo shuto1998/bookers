6c662038adc31f11c2d2c838a1c9565091b5aed2
shuto1998 <shuto.y0319@gmail.com>
2023-02-22 14:59:40 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :new
